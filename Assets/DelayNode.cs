0b0b00beb1693abac55932b93f9399f3fba5af50
Keijiro Takahashi <keijiro@gmail.com>
2016-05-25 22:36:58 +0900

    Initial commit

---
+++ Assets/DelayNode.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class DelayNode : MonoBehaviour
+{
+    [SerializeField]
+    float _delay = 1;
+
+    [SerializeField]
+    UnityEvent _event;
+
+    [SerializeField, HideInInspector]
+    Rect _editorRect;
+
+    public void Bang()
+    {
+        StartCoroutine(StartDelay());
+    }
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(_delay);
+        _event.Invoke();
+    }
+}
